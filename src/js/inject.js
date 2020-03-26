3ec04f8b4a4ff2fc4c735c572a37967c5d2e8796
Ryan Meyers <ryanmeyersweb@gmail.com>
2020-03-26 11:49:16 -0500

    Initial commit

---
+++ src/js/inject.js
@@ -0,0 +1,26 @@
+function injectScript(file_path, tag='html', type='script', text='') {
+    
+    var node = document.getElementsByTagName(tag)[0];
+    var tag_type = type == 'link' ? 'link' : 'script';
+    var script = document.createElement(tag_type);
+    if(type == 'script') {
+    script.setAttribute('type', 'text/javascript');
+    }
+    else if ( type == 'module' ) {
+    script.setAttribute('type', 'module');
+    }
+    else {
+        script.setAttribute('rel', 'stylesheet');
+        script.setAttribute('media', 'screen');
+        
+    }
+    if (text == '') {
+        script.setAttribute(tag_type == 'script' ? 'src': 'href', file_path);
+    }
+    else {
+        script.innerHTML = text;
+    }
+    node.appendChild(script);
+}
+
+injectScript(chrome.runtime.getURL('js/grid.js'));
